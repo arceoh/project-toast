aa6d7a945d4c006ce10081bfe6c23182ea1ca3b7
arceoh <arceoh@gmail.com>
2024-07-22 20:29:24 -0700

    Refactor to use context for toastList.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import Button from "../Button";
 
-import styles from "./ToastPlayground.module.css";
-import Toast from "../Toast/Toast";
-import ToastShelf from "../ToastShelf/ToastShelf";
 import Header from "../Header/Header";
+import ToastShelf from "../ToastShelf/ToastShelf";
+import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
+export const ToastListContext = React.createContext()
 
 function ToastPlayground() {
   const [toastMessage, setToastMessage] = React.useState("");
@@ -31,83 +31,77 @@ function ToastPlayground() {
 
   }
 
-  const handleRemoveToast = React.useCallback((id) => {
-    if (!id) return;
-    setToastList((prevToastList) =>
-      prevToastList.filter((toast) => toast.id !== id)
-    );
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
   }
 
   return (
-    <div className={styles.wrapper}>
-      <Header />
-      <ToastShelf>
-        {toastList.map(toast => {
-          return <Toast handleRemoveToast={handleRemoveToast} key={toast.id} toast={toast}>{toast.message}</Toast>
-        })}
-      </ToastShelf>
+    <ToastListContext.Provider value={{ toastList, setToastList }} >
+      <div className={styles.wrapper}>
+        <Header />
+        <ToastShelf />
 
-      <div className={styles.controlsWrapper}>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.row}>
-            <label
-              htmlFor="message"
-              className={styles.label}
-              style={{ alignSelf: "baseline" }}
-            >
-              Message
-            </label>
-            <div className={styles.inputWrapper}>
-              <textarea
-                id="message"
-                value={toastMessage}
-                onChange={(e) => {
-                  setToastMessage(e.target.value);
-                }}
-                className={styles.messageInput}
-              />
-            </div>
-          </div>
 
-          <div className={styles.row}>
-            <div className={styles.label}>Variant</div>
-            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-              {VARIANT_OPTIONS.map((option) => {
-                return (
-                  <label key={option} htmlFor={`variant-${option}`}>
-                    <input
-                      id={`variant-${option}`}
-                      type="radio"
-                      name="variant"
-                      value={option}
-                      checked={option === toastVariant}
-                      onChange={(e) => {
-                        setToastVariant(e.target.value);
-                      }}
-                    />
-                    {option}
-                  </label>
-                );
-              })}
-            </div>
-          </div>
 
-          <div className={styles.row}>
-            <div className={styles.label} />
-            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-              <Button
-                onClick={handleAddToast}>
-                Pop Toast!
-              </Button>
+        <div className={styles.controlsWrapper}>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.row}>
+              <label
+                htmlFor="message"
+                className={styles.label}
+                style={{ alignSelf: "baseline" }}
+              >
+                Message
+              </label>
+              <div className={styles.inputWrapper}>
+                <textarea
+                  id="message"
+                  value={toastMessage}
+                  onChange={(e) => {
+                    setToastMessage(e.target.value);
+                  }}
+                  className={styles.messageInput}
+                />
+              </div>
             </div>
-          </div>
-        </form>
+
+            <div className={styles.row}>
+              <div className={styles.label}>Variant</div>
+              <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+                {VARIANT_OPTIONS.map((option) => {
+                  return (
+                    <label key={option} htmlFor={`variant-${option}`}>
+                      <input
+                        id={`variant-${option}`}
+                        type="radio"
+                        name="variant"
+                        value={option}
+                        checked={option === toastVariant}
+                        onChange={(e) => {
+                          setToastVariant(e.target.value);
+                        }}
+                      />
+                      {option}
+                    </label>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className={styles.row}>
+              <div className={styles.label} />
+              <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+                <Button
+                  onClick={handleAddToast}>
+                  Pop Toast!
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </ToastListContext.Provider>
   );
 }
 
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 import styles from "./ToastShelf.module.css";
+import Toast from "../Toast/Toast";
+import { ToastListContext } from "../ToastPlayground/ToastPlayground";
 
-function ToastShelf({ children }) {
+function ToastShelf() {
+  const { toastList, setToastList } = React.useContext(ToastListContext)
 
-  return (<ol className={styles.wrapper}>{children}</ol>)
+
+  const handleRemoveToast = React.useCallback((id) => {
+    if (!id) return;
+    setToastList((prevToastList) =>
+      prevToastList.filter((toast) => toast.id !== id)
+    );
+  }, []);
+
+  return (<ol className={styles.wrapper}> {toastList.map(toast => {
+    return <Toast handleRemoveToast={handleRemoveToast} key={toast.id} toast={toast}>{toast.message}</Toast>
+  })}</ol>
+
+
+  )
 
 }
 
